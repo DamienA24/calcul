16c7cd1bec272a9b7c4360bbae208a7c1a78aceb
DamienA24 <alves.d24@laposte.net>
2024-09-05 18:55:14 +0200

    test

--- components/ui/bannerAds.tsx
+++ components/ui/bannerAds.tsx
@@ -4,42 +4,38 @@ import { HTMLAttributes, useEffect, useRef } from "react";
 
 const BannerAds = (props: HTMLAttributes<HTMLDivElement>) => {
   const { className } = props;
-  const container = useRef<HTMLDivElement>(null);
+  const banner = useRef<HTMLDivElement>(null);
 
   const isWindow = typeof window !== "undefined";
 
   const atOptions = {
     key: "d6f6568aafbe19353e2438fa0cea8c4a",
     format: "iframe",
-    height: 60,
-    width: 468,
+    height: 50,
+    width: 320,
     params: {},
   };
 
   useEffect(() => {
     console.log("icic");
-    if (isWindow && container.current && !container.current.firstChild) {
+    if (isWindow && banner.current && !banner.current.firstChild) {
       console.log("passe");
 
       const conf = document.createElement("script");
       const script = document.createElement("script");
       script.type = "text/javascript";
       script.src = `//pl24277107.cpmrevenuegate.com/${atOptions.key}/invoke.js`;
-      script.async = true;
-      script.defer = true;
-      conf.async = true;
-      conf.defer = true;
       conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
 
-      container.current.append(conf);
-      container.current.append(script);
+      banner.current.append(conf);
+      banner.current.append(script);
     }
-  }, [container, isWindow]);
+  }, [banner, isWindow]);
 
   return (
     <div
-      ref={container}
-      className=" super-test flex items-center justify-center"
+      ref={banner}
+      className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center"
     />
   );
 };
