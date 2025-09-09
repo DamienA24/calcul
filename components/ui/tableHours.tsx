c11638e74733970a66554d7622fcf5012b3cbeec
DamienA <alves.d24@laposte.net>
2025-09-09 11:39:12 +0200

    fix(tableHours): add function to update slots to print

--- components/ui/tableHours.tsx
+++ components/ui/tableHours.tsx
@@ -78,6 +78,7 @@ export default function TableHours() {
   const removeSlot = (id: number) => {
     const newSlot = slots.filter((slot) => slot.id !== id);
     setSlots(newSlot);
+    setSlotsToPrint(newSlot);
     calculateTotalTime(newSlot);
   };
 
